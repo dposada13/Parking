140a6e08b9b10e606c2c4d7d9c41ee69f015b2ec
Daniel Posada <dposada13@gmail.com>
2020-11-15 15:00:26 -0500

    Clase Carro 20201115

---
+++ Carro.java
@@ -0,0 +1,8 @@
+
+public class Carro extends Vehiculo
+{
+    public Carro(String placa,String marca,String color,String f,String tipoV, int valorComercial)
+    {
+        super (placa,marca,color,f,tipoV,valorComercial);    
+    }
+}
